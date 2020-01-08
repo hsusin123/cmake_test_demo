b6d057272d335873fe771fb1183e656a0eda0bf5
xuxin <xuxin@momenta.ai>
2020-01-08 17:25:38 +0800

    add ext mod

---
+++ src/ext_mod/src/source_ext.cpp
@@ -0,0 +1,4 @@
+#include "ext_mod/header_ext.hpp"
+#include <iostream>
+
+void ext_pf() { std::cout << "this is ext_pf()" << std::endl; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#include "ext_mod/header_ext.hpp"
 #include "sub_mod1/import1.h"
 #include "version.h"
 #include <iostream>
 int main() {
   std::cout << "hello world" << std::endl;
   sub_mod1_pf();
+  ext_pf();
   std::cout << "version is " << VERSION << std::endl;
 }
